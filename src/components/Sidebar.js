5ada9b52d41e041c2e663ac359850415e20eb95d
Robson Correia <robsoncorreia@192.168.100.5>
2023-07-18 16:51:15 -0300

    feat: add Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { Link } from 'react-router-dom';
+
+import { IoMdArrowForward } from 'react-icons/io'
+import { FiTrash2 } from 'react-icons/fi'
+
+import CardItem from '../components/CartItem'
+import { SidebarContext } from '../contexts/SidebarContext';
 
 const Sidebar = () => {
+  const { isOpen, handleClose } = useContext(SidebarContext)
+
   return <div>Sidebar</div>;
 };
 
